5861e75ec6cadc92cf2e777b4a79664c3cd3fe98
Anna Varetsa <koran@list.ru>
2024-11-13 16:17:14 +0300

    Работа по 9 модулю

---
+++ index.js
@@ -0,0 +1,18 @@
+const a = 15;
+console.log(a);
+
+// myUndefinedFunction();
+
+import * as Sentry from '@sentry/browser';
+
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+
+    dsn: 'YOUR_SENTRY_DSN',
+
+    integrations: [new Integrations.BrowserTracing()],
+
+    tracesSampleRate: 1.0,
+
+});
